59b1a424a83e5b26464c0b04430870c86e687767
lukasz-ciupek <lukasz.ciupek@o2.pl>
2019-11-09 09:13:34 +0100

    add EmployeeRepositoryTest

---
+++ src/main/java/pl/team/carrent/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package pl.team.carrent.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.team.carrent.model.Role;
+
+import java.util.List;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    List<Role> findByAuthority(String authority);
+}
