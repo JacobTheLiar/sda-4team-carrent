4be72d17642eb77645b0fbec4aede1d19ce05663
jakub <jacob_the_liar@tlen.pl>
2019-11-29 17:28:17 +0100

    added invalidate session on logout

--- src/main/java/pl/team/carrent/config/SecurityConfig.java
+++ src/main/java/pl/team/carrent/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package pl.team.carrent.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,8 +9,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import pl.team.carrent.employee.EmployeeService;
 
 
@@ -22,70 +21,89 @@ import pl.team.carrent.employee.EmployeeService;
  * *
  ******************************************************/
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    private final EmployeeService employeeService;
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    
+    private final EmployeeService                      employeeService;
     private final EmployeeAuthenticationSuccessHandler successHandler;
-
-    public SecurityConfig(EmployeeService employeeService, EmployeeAuthenticationSuccessHandler successHandler) {
+    
+    
+    public SecurityConfig(EmployeeService employeeService, EmployeeAuthenticationSuccessHandler successHandler){
         this.employeeService = employeeService;
         this.successHandler = successHandler;
     }
-
+    
+    
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf()
+                .disable()
                 .authorizeRequests()
-                    .antMatchers("/login").permitAll()
-                    .antMatchers("/h2-console/**").permitAll()
-                    .antMatchers("/").hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
-                    .antMatchers("/client").hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
-                    .antMatchers("/rent").hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
-                    .antMatchers("/rent/history").hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
-                    .antMatchers("/invoice").hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
-                    .antMatchers("/car").hasAnyAuthority("ADMIN", "SUPERUSER")
-                    .antMatchers("/car/model").hasAnyAuthority("ADMIN", "SUPERUSER")
-                    .antMatchers("/promotion").hasAnyAuthority("ADMIN", "SUPERUSER")
-                    .antMatchers("/employee").hasAuthority("ADMIN")
-                    .antMatchers("/rentPoint").hasAuthority("ADMIN")
-                    .antMatchers("/**").authenticated()
+                .antMatchers("/login")
+                .permitAll()
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .antMatchers("/")
+                .hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
+                .antMatchers("/client")
+                .hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
+                .antMatchers("/rent")
+                .hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
+                .antMatchers("/rent/history")
+                .hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
+                .antMatchers("/invoice")
+                .hasAnyAuthority("ADMIN", "SUPERUSER", "USER")
+                .antMatchers("/car")
+                .hasAnyAuthority("ADMIN", "SUPERUSER")
+                .antMatchers("/car/model")
+                .hasAnyAuthority("ADMIN", "SUPERUSER")
+                .antMatchers("/promotion")
+                .hasAnyAuthority("ADMIN", "SUPERUSER")
+                .antMatchers("/employee")
+                .hasAuthority("ADMIN")
+                .antMatchers("/rentPoint")
+                .hasAuthority("ADMIN")
+                .antMatchers("/**")
+                .authenticated()
                 .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .usernameParameter("username")
-                    .passwordParameter("password")
-//                .successHandler(successHandler())
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                //.successHandler(successHandler())
                 .successHandler(successHandler)
                 .and()
-                    .exceptionHandling()
-                    .accessDeniedPage("/accessDenied")
+                .exceptionHandling()
+                .accessDeniedPage("/accessDenied")
                 .and()
-                .headers().frameOptions().disable()
+                .headers()
+                .frameOptions()
+                .disable()
                 .and()
                 .logout()
+                .invalidateHttpSession(true)
                 .logoutSuccessUrl("/login?logout");
     }
-
+    
+    
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.userDetailsService(employeeService).passwordEncoder(passwordEncoder());
     }
-
+    
+    
     @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
     }
-
-//    @Bean
-//    AuthenticationSuccessHandler successHandler() {
-//        return new SavedRequestAwareAuthenticationSuccessHandler();
-//    }
-
+    
+    //    @Bean
+    //    AuthenticationSuccessHandler successHandler() {
+    //        return new SavedRequestAwareAuthenticationSuccessHandler();
+    //    }
+    
+    
     @Bean
-    PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 }
